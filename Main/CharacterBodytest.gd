25168f287e0815caebb7ec4ba9f5bba8275d1fff
Zeepnely <kamheater.55@gmail.com>
2024-05-25 10:39:01 -0700

    more stuvs

--- Main/CharacterBodytest.gd
+++ Main/CharacterBodytest.gd
@@ -1,9 +1,10 @@
 extends CharacterBody3D
 
-
+const sensetivity = .01
 const SPEED = 5.0
 const JUMP_VELOCITY = 4.5
-
+@onready var Head1 = $"head one"
+@onready var Camra = $"head one/Camera3D"
 # Get the gravity from the project settings to be synced with RigidBody nodes.
 var gravity = (9.8)
 func _input(event):
@@ -12,7 +13,11 @@ func _input(event):
 
 func _ready():
 	Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
-
+	
+func _unhandled_input(event):
+	if event is InputEventMouseMotion:
+		Head1.rotate_y(-event.relative.x * sensetivity)
+		Camra.rotate_x(event.relative.y * sensetivity)
 func _physics_process(delta):
 	# Add the gravity.
 	if not is_on_floor():
@@ -25,12 +30,12 @@ func _physics_process(delta):
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
 	var input_dir = Input.get_vector("left", "right", "backword", "forword")
-	var direction = (transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
+	var direction = (Head1.transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
 	if direction:
 		velocity.x = direction.x * SPEED
 		velocity.z = direction.z * SPEED
 	else:
-		velocity.x = move_toward(velocity.x, 0, SPEED)
-		velocity.z = move_toward(velocity.z, 0, SPEED)
+		velocity.x = 0
+		velocity.z = 0
 
 	move_and_slide()
