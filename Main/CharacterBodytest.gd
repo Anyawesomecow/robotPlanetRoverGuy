74cc36dd4c01690b7aa83c78c0cf858614ba9186
Zeepnely <kamheater.55@gmail.com>
2024-05-25 19:13:59 -0700

    wooo i added a slam(cool)

--- Main/CharacterBodytest.gd
+++ Main/CharacterBodytest.gd
@@ -1,15 +1,40 @@
 extends CharacterBody3D
-
+var legtype
 const sensetivity = .01
-const SPEED = 5.0
+const SPEED = 8.0
 const JUMP_VELOCITY = 4.5
 @onready var Head1 = $"head one"
 @onready var Camra = $"head one/Camera3D"
-# Get the gravity from the project settings to be synced with RigidBody nodes.
-var gravity = (9.8)
+var gravity = 9.8
+enum legset {legs, roller, treds,}
+var temphold = 0
+func add(num1, num2):
+	var result = num1 + num2
+	return result
+
 func _input(event):
+	if event.is_action_pressed("abilitylegs"):
+		velocity.y = -20
 	if event.is_action_pressed("esc"):
 		get_tree().quit()
+	if event.is_action_pressed("switchLegs1"):# temp leg swap
+		temphold += 1
+		if temphold == 0:
+			print(legset.legs)
+			legtype = legset.legs
+		if temphold == 1:
+			print(legset.roller)
+			legtype = legset.roller
+		if temphold == 2:
+			print(legset.treds)
+			legtype = legset.treds
+		if temphold ==4 or temphold > 4:
+			temphold = 0
+		print("bob")
+		print(temphold)
+
+
+
 
 func _ready():
 	Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
@@ -25,7 +50,8 @@ func _physics_process(delta):
 
 	# Handle jump.
 	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
-		velocity.y = JUMP_VELOCITY
+		velocity.y = JUMP_VELOCITY * 2
+		print("bob")
 
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
