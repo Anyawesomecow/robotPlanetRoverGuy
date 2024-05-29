a4fef4eb4f42f38cb82d966a4f9c8bf9711b23da
Zeepnely <kamheater.55@gmail.com>
2024-05-28 20:51:26 -0700

    uh oh

--- Main/UIs/ui_pausemenue.gd
+++ Main/UIs/ui_pausemenue.gd
@@ -1,15 +1,23 @@
 extends Control
+var paused = false
 
 func open():
 	Input.set_mouse_mode(Input.MOUSE_MODE_VISIBLE)
-	show()
-	get_tree().Paused = true
+	$".".show()
+	get_tree().paused = true
+	paused = true
 
 
-func UnPause():
+func close():
+	Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
 	$".".hide()
-	get_tree().Paused = false
-
+	get_tree().paused = false
+	paused = false
+	
 func _input(event):
+	print("blorguse")
 	if event.is_action_pressed("esc"):
-		UnPause()
+		if paused:
+			close()
+		else:
+			open()
--- Main/node_3d.gd
+++ Main/node_3d.gd
@@ -1,5 +1,9 @@
 extends Node3D
 
 func _input(event):
-	if event.is_action_pressed("esc") and :
-		$in_game_menue.open()
+	if event.is_action_pressed("esc"):
+		if event.is_action_pressed("esc"):
+			if $in_game_menue.paused:
+				$in_game_menue.close()
+			else:
+				$in_game_menue.open()
